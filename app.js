b791010b61723f742f8222685aca5e21a93e2fda
d0177381@gmail.com <>
2020-09-27 02:17:04 +0800

    init

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { appPort } = process.env;
 const app = express();
 app.use(Have.haven());
 
-app.use('/', require('./src/routes/index'));
+app.use('/api', require('./src/routes/index'));
 app.listen(appPort, function() {
   console.log(`Love-Never-Stray on port: ${appPort}`);
 });
