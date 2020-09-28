ef4b3361d4e8028bbd3b12d2a3486558bdd81bbf
d0177381@gmail.com <>
2020-09-28 23:06:10 +0800

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,23 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true,
-    jest: true
+    node: true
   },
-  extends: ['standard'],
+  extends: [
+    'standard'
+  ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
   },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module'
   },
+  plugins: [
+    '@typescript-eslint'
+  ],
   rules: {
     camelcase: 'off',
     'no-tabs': 0,
