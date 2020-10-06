c9e9eb0a2e81101f1787bc0ea1ebb1e0a3421f12
d0177381@gmail.com <>
2020-10-06 23:45:51 +0800

    [Remove] typescript settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,23 +2,18 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
+    jest: true
   },
-  extends: [
-    'standard'
-  ],
+  extends: ['standard'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
   },
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module'
   },
-  plugins: [
-    '@typescript-eslint'
-  ],
   rules: {
     camelcase: 'off',
     'no-tabs': 0,
