fb11824f54cebb8f9d63b2932e017fae9983ab9c
d0177381@gmail.com <>
2020-10-23 00:54:30 +0800

    [Remove] sync pets table

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const Pet = require('../schemas/pets');
 const router = express.Router();
 
 router.get('/', async function(req, res) {
-  await Pet.sync({ force: true });
   res.json('Hello, my ec2');
 });
 
