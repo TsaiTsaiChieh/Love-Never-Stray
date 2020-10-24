cd61fed6c5d95cac5cea45f3c93ba81dcb526408
d0177381@gmail.com <>
2020-10-24 19:43:40 +0800

    not finish

--- schedulers.js
+++ schedulers.js
@@ -3,13 +3,15 @@ const express = require('express');
 const Haven = require('domain-haven');
 const scheduler = require('node-schedule');
 const { schedulerPort } = process.env;
-const COA = require('./src/schedulers/COA');
+// const COA = require('./src/schedulers/COA');
+const meetPets = require('./src/schedulers/meetPets');
 
 const app = express();
 app.use(Haven.haven());
 
 scheduler.scheduleJob('*/5 * * * * *', async function() {
-  await COA();
+  // await COA();
+  await meetPets();
 });
 
 app.listen(schedulerPort, function() {
