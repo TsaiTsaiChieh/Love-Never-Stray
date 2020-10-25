441b9b0c9824d5d3ef1ebd1ea55625f9e0c6f4ff
d0177381@gmail.com <>
2020-10-25 21:39:34 +0800

    [Add] upsertPetTable function

--- src/helpers/databaseEngine.js
+++ src/helpers/databaseEngine.js
@@ -3,9 +3,10 @@ const Pet = require('../schemas/pets');
 
 async function upsertPetTable(data) {
   try {
-    console.log(data);
-    const result = await Pet.upsert(data, { where: data.id });
-    return Promise.resolve(result);
+    data.map(async(ele) => {
+      await Pet.upsert(ele, { where: ele.id });
+    });
+    return Promise.resolve();
   } catch (err) {
     return Promise.reject(new ServerErrors.MySQLError(err.stack));
   }
