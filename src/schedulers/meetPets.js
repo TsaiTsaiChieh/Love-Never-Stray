c51cb188d33dd73b1df11fb31e76505d48616633
d0177381@gmail.com <>
2020-10-26 00:51:25 +0800

    [Finish] meetPet crawler scheduler

--- src/schedulers/meetPets.js
+++ src/schedulers/meetPets.js
@@ -1,22 +1,21 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const ServerErrors = require('../helpers/ServerErrors');
+const Pet = require('../schemas/pets');
 const { meetPets_URL } = process.env;
 const className = '.field-field';
-const { statusMapping, areaMapping, ageMapping, ligationMapping } = require('../helpers/mapping');
-// const { upsertPetTable } = require('../helpers/databaseEngine');
+const { petStatus, statusMapping, areaMapping, ageMapping, ligationMapping } = require('../helpers/mapping');
+const { upsertPetTable } = require('../helpers/databaseEngine');
 
 async function main() {
   try {
     const { petTypes, petLastPages } = await getPetsLastPage();
     const petIds = await getPetIds(petTypes, petLastPages);
-    console.log(petIds);
-    // const a = petIds.splice(0, 5);
-    // const petIds = ['76502'];
+    const petIdsFromMySQL = await getPetIdsFromMySQL();
+    await updatePetStatus(petIdsFromMySQL, petIds);
     const data = await getPetInformation(petIds);
-    console.log(data, '??');
-    // const a = await upsertPetTable(data);
-
+    await upsertPetTable(data);
+    console.log('完成更新台灣認養地圖');
     return Promise.resolve();
   } catch (err) {
     return Promise.reject(err.stack);
@@ -73,36 +72,42 @@ async function getPetIds(petTypes, petLastPages) {
   }
 }
 
+async function getPetIdsFromMySQL() {
+  try {
+    const result = await Pet.findAll({
+      attributes: ['id'],
+      where: { ref: 'map' },
+      raw: true
+    });
+    return Promise.resolve(result);
+  } catch (err) {
+    return Promise.reject(new ServerErrors.MySQLError(err.stack));
+  }
+}
+
 async function getPetInformation(petIds) {
   return new Promise(function(resolve, reject) {
     try {
       const data = [];
       let currentIdx = 0;
       const intervalId = setInterval(async function() {
-        for (let i = 0; i < petIds.length; i++) {
-          const petId = petIds[i];
-          const URL = `${meetPets_URL}/content/${petId}`;
-          const $ = await loadData(URL);
-          const result = await repackagePetInformation($, petId);
-          data.push(result);
-          currentIdx++;
-          console.log(`${currentIdx}/${petIds.length}`);
-          if (currentIdx === petIds.length) {
-            clearInterval(intervalId);
-            return resolve(data);
-          }
+        const petId = petIds[currentIdx];
+        const URL = `${meetPets_URL}/content/${petId}`;
+        const $ = await loadData(URL);
+        const result = await repackagePetInformation($, petId);
+        data.push(result);
+        currentIdx++;
+        console.log(`${currentIdx}/${petIds.length}`);
+        if (currentIdx === petIds.length) {
+          clearInterval(intervalId);
+          return resolve(data);
         }
-      }, 1000);
+      }, 100);
     } catch (err) {
-      // return Promise.reject(new ServerErrors.GetDataFromURL(err.stack));
       return reject(new ServerErrors.GetDataFromURL(err.stack));
     }
   });
 }
-
-// async function later(delay, callback) {
-//   return Promise.resolve(setTimeout(callback, delay * 1000));
-// }
 
 async function repackagePetInformation($, petId) {
   try {
@@ -116,14 +121,14 @@ async function repackagePetInformation($, petId) {
       ref: 'map',
       status: statusMapping(title),
       title,
-      type: textReplace($, 'pets-type', '動物種類') === '貓咪' ? '貓' : '狗',
+      kind: textReplace($, 'pets-type', '動物種類') === '貓咪' ? '貓' : '狗',
       name: textReplace($, 'pet-name', '動物小名'),
       area_id: areaMapping(textReplace($, 'county', '所在縣市')),
       found_place: textReplace($, 'filed-county-area', '所在 區 / 市 / 鎮 / 鄉 (行政區)'),
       age: ageMapping(textReplace($, 'pet-age', '動物的出生日（年齡）')),
       ligation: ligationMapping(textReplace($, 'pet-medical', '結紮情況')),
       remark: textReplace($, 'pet-habitate', '動物個性略述'),
-      image: JSON.stringify(images),
+      image: images,
       address: textReplace($, 'contact', '聯絡人'),
       phone: textReplace($, 'tel', '電話/聯絡方式')
     };
@@ -135,6 +140,19 @@ async function repackagePetInformation($, petId) {
 
 function textReplace($, name, replace) {
   return $(`${className}-${name}`).text().replace(`${replace}:`, '').trim();
+}
+
+function updatePetStatus(idFromMySQL, data) {
+  try {
+    idFromMySQL.map(async function(ele) {
+      if (!data.includes(String(ele.id))) {
+        await Pet.update({ status: petStatus.NONE }, { where: { id: ele.id } });
+      }
+    });
+    return Promise.resolve();
+  } catch (err) {
+    return Promise.reject(new ServerErrors.MySQLError(err.stack));
+  }
 }
 
 module.exports = main;
