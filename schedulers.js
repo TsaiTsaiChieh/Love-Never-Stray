732ff2d0c4497428cb16a3c2d429393445373d3d
d0177381@gmail.com <>
2020-10-26 22:05:33 +0800

    [Change] node-schedule to node-schdule-tz

--- schedulers.js
+++ schedulers.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const Haven = require('domain-haven');
-const scheduler = require('node-schedule');
-const { schedulerPort } = process.env;
+const scheduler = require('node-schedule-tz');
+const { schedulerPort, ZONE_TW } = process.env;
 // const COA = require('./src/schedulers/COA');
 const meetPets = require('./src/schedulers/meetPets');
 
 const app = express();
 app.use(Haven.haven());
 
-scheduler.scheduleJob('* * 6 * * *', async function() {
+scheduler.scheduleJob('更新 from 台灣認養地圖', '* * 6 * * *', ZONE_TW, async function() {
   // await COA();
   await meetPets();
 });
