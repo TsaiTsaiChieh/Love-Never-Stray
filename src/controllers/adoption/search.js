c006c2e8d0fc0c058e0ad572b00dd5bbc182bac9
d0177381@gmail.com <>
2020-10-27 00:15:06 +0800

    [Add] adoption/search controller

---
+++ src/controllers/adoption/search.js
@@ -0,0 +1,43 @@
+const ajv = require('../../helpers/ajvUtil');
+const { BAD_REQUEST } = require('http-status');
+const model = require('../../models/adoption/search');
+
+async function controller(req, res) {
+  const schema = {
+    type: 'object',
+    properties: {
+      kind: {
+        type: 'string',
+        enum: ['C', 'D', 'O']
+      },
+      sex: {
+        type: 'string',
+        enum: ['0', '1']
+      },
+      age: {
+        type: 'string',
+        enum: ['0', '1']
+      },
+      time: {
+        type: 'string',
+        enum: ['DESC', 'ASC'],
+        default: 'ASC'
+      },
+      region: {
+        type: 'string',
+        enum: ['N', 'M', 'S', 'E', 'O']
+      }
+    }
+  };
+  const valid = ajv.validate(schema, req.query);
+  if (!valid) return res.status(BAD_REQUEST).json(ajv.errors);
+  try {
+    return res.json(await model(req.query));
+  } catch (err) {
+    return res.status(err.code).json(err.isPublic
+      ? { error: err.name, devcode: err.status, message: err.message }
+      : err.code);
+  }
+}
+
+module.exports = controller;
